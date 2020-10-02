76332aefb390f86edbc8cfceb9d595699eb6d5d4
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-10-02 16:11:50 +0100

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,37 @@
+/*
+    How to give Input: (examples)
+    2 + 5
+    5 * 10
+*/
+
+#include <iostream>
+using namespace std;
+
+void calculate(float x, char C, float y);
+
+int main()
+{
+    float a, b;
+    char c;
+    
+    // 2 + 5 (format)
+    cout << "Enter two values & operator: ";
+    cin >> a >> c >> b;
+    
+    cout << "\nResult: ";
+    calculate(a,c,b);
+    
+    return 0;
+}
+
+void calculate(float x, char C, float y)
+{
+    switch(C)
+    {
+        case '+': cout << (x+y); break;
+        case '-': cout << (x-y); break;
+        case '*': cout << (x*y); break;
+        case '/': cout << (x/y); break;
+        default : cout << ("Invalid Operator");
+    }
+}
