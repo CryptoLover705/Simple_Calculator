3c480e1b200dff83efe23d45a57fb429d28db0ac
Nugetzrul3 <52128634+Nugetzrul3@users.noreply.github.com>
2020-11-02 15:10:57 +1100

    Add power function

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,6 +5,7 @@
 */
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 void calculate(float x, char C, float y);
@@ -13,25 +14,36 @@ int main()
 {
     float a, b;
     char c;
-    
+
     // 2 + 5 (format)
     cout << "Enter two values & operator: ";
     cin >> a >> c >> b;
-    
+
     cout << "\nResult: ";
-    calculate(a,c,b);
-    
+    calculate(a, c, b);
+
     return 0;
 }
 
 void calculate(float x, char C, float y)
 {
-    switch(C)
+    switch (C)
     {
-        case '+': cout << (x+y); break;
-        case '-': cout << (x-y); break;
-        case '*': cout << (x*y); break;
-        case '/': cout << (x/y); break;
-        default : cout << ("Invalid Operator");
+    case '+': 
+        cout << (x + y); 
+        break;
+    case '-': 
+        cout << (x - y); 
+        break;
+    case '*': 
+        cout << (x * y); 
+        break;
+    case '/': 
+        cout << (x / y); 
+        break;
+    case '^': 
+        cout << (pow(x, y));
+        break;
+    default: cout << ("Invalid Operator");
     }
 }
